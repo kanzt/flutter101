d15f31938b1ce7f2865947932fd56f20547bac1d
KANTAPHAT TAVELERTSOPON (CDGS/ADD) <kantaphat.t@cdg.co.th>
2021-04-21 10:29:09 +0700

    Finish day 2 morning

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter101/src/config/app_route.dart';
+import 'package:flutter101/src/constants/api.dart';
 import 'package:flutter101/src/constants/app_setting.dart';
+import 'package:flutter101/src/constants/asset.dart';
+import 'package:flutter101/src/model/product_response.dart';
 import 'package:flutter101/src/pages/login/background_theme.dart';
+import 'package:flutter101/src/service/network.dart';
 import 'package:flutter101/src/view_models/menu_viewmodel.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -21,17 +26,43 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       drawer: CommonDrawer(),
       appBar: AppBar(
         title: Text('Home Page'),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
-        itemBuilder: (context, index) => ShopListItem(200),
-        itemCount: 200,
-      ),
+      body: FutureBuilder<List<ProductResponse>>(
+          future: NetworkService().productAll(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+
+            final productList = snapshot.data;
+
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.8,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+              ),
+              itemBuilder: (context, index) => LayoutBuilder(
+                builder: (context, constraint) => ShopListItem(
+                  constraint.maxHeight,
+                  productList[index],
+                  press: () {},
+                ),
+              ),
+              itemCount: productList.length,
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
@@ -96,11 +127,14 @@ class CommonDrawer extends StatelessWidget {
 class ShopListItem extends StatelessWidget {
   final Function press;
   final double maxHeight;
+  final ProductResponse product;
 
-  const ShopListItem(this.maxHeight, {Key key, this.press}) : super(key: key);
+  const ShopListItem(this.maxHeight, this.product, {Key key, this.press})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    /// GestureDetector ใช้กรณีที่ต้องการให้ Widget สามารถกดได้
     return GestureDetector(
       onTap: press,
       child: Container(
@@ -124,7 +158,7 @@ class ShopListItem extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
-              'ชื่อสินค้า',
+              product.name,
               overflow: TextOverflow.ellipsis,
               maxLines: 2,
             ),
@@ -132,13 +166,13 @@ class ShopListItem extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Text(
-                  '\$ ราคาสินค้า',
+                  '\$ ${product.price}',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 Text(
-                  'จำนวน',
+                  '${product.stock} pieces',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     color: Colors.deepOrangeAccent,
@@ -151,55 +185,56 @@ class ShopListItem extends StatelessWidget {
       );
 
   Stack _buildImage() {
-    final height = maxHeight - 75;
-    final productImage =
-        'https://shortrecap.co/wp-content/uploads/2020/05/Catcover_web.jpg';
+    final height = maxHeight * 0.7;
+    final productImage = product.image;
     return Stack(
       children: [
         productImage != null && productImage.isNotEmpty
             ? Image.network(
-                productImage,
+                '${API.IMAGE_URL}/$productImage',
                 height: height,
                 width: double.infinity,
                 fit: BoxFit.cover,
               )
             : Image.asset(
-                'xxx',
+                Asset.noPhotoImage,
                 height: height,
                 width: double.infinity,
               ),
-        1 > 0
-            ? SizedBox()
-            : Positioned(
-                top: 1,
-                right: 1,
-                child: Card(
-                  color: Colors.white70,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8,
-                      vertical: 4,
+        // 1 > 0
+        //     ? SizedBox()
+        //     :
+        if (product.stock <= 0)
+          Positioned(
+            top: 1,
+            right: 1,
+            child: Card(
+              color: Colors.white70,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 4,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      FontAwesomeIcons.box,
+                      size: 15.0,
+                      color: Colors.black,
                     ),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          FontAwesomeIcons.box,
-                          size: 15.0,
-                          color: Colors.black,
-                        ),
-                        SizedBox(width: 4),
-                        Text(
-                          'out of stock',
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
+                    SizedBox(width: 4),
+                    Text(
+                      'out of stock',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ),
+            ),
+          ),
       ],
     );
   }
---
+++ lib/src/service/network.dart
@@ -0,0 +1,121 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:flutter101/src/constants/api.dart';
+import 'package:flutter101/src/model/product_response.dart';
+
+class NetworkService {
+  NetworkService._internal();
+
+  static final NetworkService _instance = NetworkService._internal();
+
+  factory NetworkService() => _instance;
+
+  static final Dio _dio = Dio()
+    ..interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (RequestOptions requestOptions) async {
+          requestOptions.baseUrl = API.BASE_URL;
+          requestOptions.connectTimeout = 5000;
+          requestOptions.receiveTimeout = 3000;
+          print(requestOptions.baseUrl);
+          print(requestOptions.path);
+          return requestOptions;
+        },
+        onResponse: (Response response) async {
+          return response;
+        },
+        onError: (DioError dioError) async {
+          switch (dioError.response.statusCode) {
+            case 301:
+              return '301';
+              break;
+            case 401:
+              return '401';
+              break;
+            case 404:
+              return '404';
+              break;
+            default:
+              return 'Network failed';
+          }
+        },
+      ),
+    );
+
+  // getDemo() async{
+  //   final response = await _dio.get('https://d92e1eb74350.jp.ngrok.io/products');
+  //   if(response.statusCode == 200){
+  //     print(response.data.toString());
+  //     return;
+  //   }
+  //   throw Exception('Network failed');
+  // }
+
+  Future<List<ProductResponse>> productAll() async {
+    final response = await _dio.get(API.PRODUCT);
+    if (response.statusCode == 200) {
+      return productResponseFromJson(json.encode(response.data));
+    }
+    throw Exception('Network failed');
+  }
+
+// Future<String> addProduct(File imageFile, Product product) async {
+//   FormData data = FormData.fromMap({
+//     'name': product.name,
+//     'price': product.price,
+//     'stock': product.stock,
+//     if (imageFile != null)
+//       'photo': await MultipartFile.fromFile(
+//         imageFile.path,
+//         contentType: MediaType('image', 'jpg'),
+//       ),
+//   });
+//
+//   final response = await _dio.post(API.PRODUCT, data: data);
+//
+//   if (response.statusCode == 201) {
+//     return 'Add Successfully';
+//   }
+//   throw Exception('Network failed');
+// }
+//
+// Future<String> editProduct(File imageFile, Product product) async {
+//   FormData data = FormData.fromMap({
+//     'name': product.name,
+//     'price': product.price,
+//     'stock': product.stock,
+//     if (imageFile != null)
+//       'photo': await MultipartFile.fromFile(
+//         imageFile.path,
+//         contentType: MediaType('image', 'jpg'),
+//       ),
+//   });
+//
+//   final response = await _dio.put('${API.PRODUCT}/${product.id}', data: data);
+//
+//   if (response.statusCode == 200) {
+//     return 'Edit Successfully';
+//   }
+//   throw Exception('Network failed');
+// }
+//
+// Future<String> deleteProduct(int id) async {
+//   final response = await _dio.delete('${API.PRODUCT}/$id');
+//
+//   if (response.statusCode == 204) {
+//     return 'Delete Successfully';
+//   }
+//   throw Exception('Network failed');
+// }
+//
+// Future<List<Posts>> fetchPosts(int startIndex, {int limit = 10}) async {
+//   final url =
+//       'https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$limit';
+//
+//   final Response response = await _dio.get(url);
+//   if (response.statusCode == 200) {
+//     return postsFromJson(jsonEncode(response.data));
+//   }
+//   throw Exception('Network failed');
+// }
+}
