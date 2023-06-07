a4ada67c6df05a92bdedb2c6e5b68665dce1aaf7
kanzt <kanzttt@gmail.com>
2023-06-07 17:05:34 +0700

    Add FlutterMqttBackgroundHandler.swift

---
+++ Plugin/flutter_mqtt/flutter_mqtt/ios/Classes/FlutterMqttBackgroundHandler.swift
@@ -0,0 +1,27 @@
+import Flutter
+
+class FlutterMqttBackgroundHandler {
+    private var onBackgroundInitialized: (() -> Void)?
+    
+    init(onBackgroundInitialized: (() -> Void)?) {
+           self.onBackgroundInitialized = onBackgroundInitialized
+       }
+    
+    func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch (call.method){
+        case "backgroundChannelInitialized" :
+            self.onBackgroundInitialized?()
+            break
+        case "getReceiveBackgroundNotificationCallbackHandle":
+            let callbackHandle = UserDefaults.standard.integer(forKey: keyReceiveBackgroundNotificationCallbackHandle)
+            if callbackHandle != 0{
+                result(callbackHandle)
+            }else{
+                result(FlutterError(code: "receive_background_notification_callback_handle_not_found"
+                                    , message: "The CallbackHandle could not be found. Please make sure it has been set when you initialize plugin", details: nil))
+            }
+            break
+        default : result(FlutterMethodNotImplemented)
+        }
+    }
+}
