4d114d811a22c6b2ff072fea861869bd47ec19a3
kanzt <kanzttt@gmail.com>
2024-04-01 17:08:16 +0700

    Added Navbar for mobile

--- flutter_web_101/lib/src/presentation/widgets/navbar.dart
+++ flutter_web_101/lib/src/presentation/widgets/navbar.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_101/src/presentation/util/responsiveLayout.dart';
 
-class NavBar extends StatelessWidget {
+class NavBar extends StatefulWidget {
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> with TickerProviderStateMixin {
   final navLinks = ["Home", "Products", "News", "Contact"];
 
-  List<Widget> navItem() {
+  bool _isNavbarOpened = false;
+  List<Widget> navItemLarge() {
     return navLinks.map((text) {
       return Padding(
         padding: const EdgeInsets.only(left: 18),
+        child: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: Text(text,
+                style: const TextStyle(fontFamily: "Montserrat-Bold"))),
+      );
+    }).toList();
+  }
+
+  List<Widget> navItemSmall() {
+    return navLinks.map((text) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
         child: MouseRegion(
             cursor: SystemMouseCursors.click,
             child: Text(text,
@@ -20,79 +38,116 @@ class NavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 38),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
+      child: Column(
+        children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(8),
-                // decoration: BoxDecoration(
-                //   borderRadius: BorderRadius.circular(18),
-                //   gradient: const LinearGradient(colors: [
-                //     Color(0xFF18341d),
-                //     Color(0xFF507d58),
-                //   ], begin: Alignment.bottomRight, end: Alignment.topLeft),
-                // ),
-                child: Image.network("assets/logo.png", width: 50, height: 50,),
-                // const Center(
-                //   child: Text(
-                //     "CDG",
-                //     style: TextStyle(fontSize: 30, color: Colors.white),
-                //   ),
-                // ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    // decoration: BoxDecoration(
+                    //   borderRadius: BorderRadius.circular(18),
+                    //   gradient: const LinearGradient(colors: [
+                    //     Color(0xFF18341d),
+                    //     Color(0xFF507d58),
+                    //   ], begin: Alignment.bottomRight, end: Alignment.topLeft),
+                    // ),
+                    child: Image.network(
+                      "assets/logo.png",
+                      width: 50,
+                      height: 50,
+                    ),
+                    // const Center(
+                    //   child: Text(
+                    //     "CDG",
+                    //     style: TextStyle(fontSize: 30, color: Colors.white),
+                    //   ),
+                    // ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  // const Text("Intranet", style: TextStyle(fontSize: 26))
+                ],
               ),
-              const SizedBox(
-                width: 16,
-              ),
-              // const Text("Intranet", style: TextStyle(fontSize: 26))
-            ],
-          ),
-          if (!ResponsiveLayout.isSmallScreen(context))
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                ...navItem(),
-                InkWell(
-                  child: MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: Container(
-                      margin: const EdgeInsets.only(left: 20),
-                      width: 120,
-                      height: 40,
-                      decoration: BoxDecoration(
-                          gradient: const LinearGradient(
-                              colors: [
-                                Color(0xFF18341d),
-                                Color(0xFF507d58),
-                              ],
-                              begin: Alignment.bottomRight,
-                              end: Alignment.topLeft),
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                                color: const Color(0xFF6078ea).withOpacity(.3),
-                                offset: const Offset(0, 8),
-                                blurRadius: 8)
-                          ]),
-                      child: const Material(
-                        color: Colors.transparent,
-                        child: Center(
-                          child: Text("Login",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  letterSpacing: 1,
-                                  fontFamily: "Montserrat-Bold")),
+              if (!ResponsiveLayout.isSmallScreen(context))
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    ...navItemLarge(),
+                    InkWell(
+                      child: MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 20),
+                          width: 120,
+                          height: 40,
+                          decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                  colors: [
+                                    Color(0xFF18341d),
+                                    Color(0xFF507d58),
+                                  ],
+                                  begin: Alignment.bottomRight,
+                                  end: Alignment.topLeft),
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                    color:
+                                        const Color(0xFF6078ea).withOpacity(.3),
+                                    offset: const Offset(0, 8),
+                                    blurRadius: 8)
+                              ]),
+                          child: const Material(
+                            color: Colors.transparent,
+                            child: Center(
+                              child: Text("Login",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      letterSpacing: 1,
+                                      fontFamily: "Montserrat-Bold")),
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                  ],
+                )
+              else
+                MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _isNavbarOpened = !_isNavbarOpened;
+                          });
+                        },
+                        child: Image.network("assets/menu.png",
+                            width: 26, height: 26)))
+            ],
+          ),
+          if (ResponsiveLayout.isSmallScreen(context))
+          Visibility(
+            visible: _isNavbarOpened,
+            maintainState: true,
+            maintainAnimation: true,
+            maintainSize: true,
+            child: AnimatedSize(
+              curve: Curves.easeOut,
+              duration: const Duration(microseconds: 500),
+              child: SizedBox(
+                height: _isNavbarOpened ? 250 : 0,
+                child: Column(
+                  children: [
+                    ...navItemSmall(),
+                  ],
                 ),
-              ],
-            )
-          else
-            Image.network("assets/menu.png", width: 26, height: 26)
+              ),
+            ),
+          ),
         ],
       ),
     );
